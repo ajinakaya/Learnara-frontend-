70045fcd43d9f209e5fc496a81b26ecdc5138730
ajinakaya <ajina.kayastha5@gmail.com>
2025-02-27 20:17:30 +0545

    adding provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { LanguageProvider } from "./private/context/languagecontext";
+import { AuthProvider } from "./private/context/authcontext";
+import {GoalProvider} from "./private/context/goalcontext";
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <Router>
+    <AuthProvider>
+    <GoalProvider>
+    <LanguageProvider>
     <App />
+    </LanguageProvider>
+    </GoalProvider>
+    </AuthProvider>
     </Router>
-  </StrictMode>,
+    
 )
